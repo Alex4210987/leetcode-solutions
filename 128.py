ed67286bd5a7716cdcbbb03cbae88ed22f01fa96
Alex4210987 <u202211967@hust.edu.cn>
2023-07-07 14:05:18 +0800

    136finished

--- 128.py
+++ 128.py
@@ -1,4 +1,15 @@
+#You must write an algorithm that runs in O(n) time.
+#Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        #You must write an algorithm that runs in O(n) time.
-       
+        nums = set(nums)#remove duplicates
+        longest = 0
+        for num in nums:
+            if num - 1 not in nums:
+                current = num
+                current_length = 1
+                while current + 1 in nums:
+                    current += 1
+                    current_length += 1
+                longest = max(longest, current_length)
+        return longest                
